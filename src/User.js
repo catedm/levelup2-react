aa9f04ddb20014bedd075120323a4aa3cb7c8e63
catedm <david@element-360.com>
2019-02-18 20:17:38 -0500

    more commits

---
+++ src/User.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { UserContext } from './UserContext';
+
+export default class User extends Component {
+  render() {
+    return (
+      <UserContext.Consumer>
+        {context => (
+        <div>
+          <h1>{context.user.name}</h1>
+        </div>
+        )}
+      </UserContext.Consumer>
+    )
+  }
+}
